2a320ff281ac6a5280a74e3435db1a49455a907a
Stepan <sfermin2007@icloud.com>
2025-10-20 11:48:48 +0200

    Initial Commit

---
+++ chickenAlarm/chickenAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  chickenAlarmApp.swift
+//  chickenAlarm
+//
+//  Created by Stepan Yarikova on 20/10/25.
+//
+
+import SwiftUI
+
+@main
+struct chickenAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chickenAlarmTests/chickenAlarmTests.swift
@@ -0,0 +1,17 @@
+//
+//  chickenAlarmTests.swift
+//  chickenAlarmTests
+//
+//  Created by Stepan Yarikova on 20/10/25.
+//
+
+import Testing
+@testable import chickenAlarm
+
+struct chickenAlarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
